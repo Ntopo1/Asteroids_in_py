6a7871a710705dc26df0a669fc01cd5fd8f07c86
Nick Topolinski <nicholastop@msn.com>
2025-04-02 11:21:44 -0400

    H: Rate Limit

--- player.py
+++ player.py
@@ -10,6 +10,8 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
         self.PLAYER_RADIUS = PLAYER_RADIUS
         self.rotation = 0
+        self.timer = 0
+        
 
 
     # Set Hit Box
@@ -34,6 +36,7 @@ class Player(CircleShape):
     
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.timer -= dt
 
         if keys[pygame.K_a]:
             self.rotate(-dt)
@@ -45,14 +48,18 @@ class Player(CircleShape):
             self.move(-dt)
         if keys[pygame.K_SPACE]:
             self.shoot()
-        
+            
+
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
+        if self.timer > 0:
+            return
+        self.timer = PLAYER_SHOOT_COOLDOWN
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
-        #shots_group.add(shot)
+        
         
